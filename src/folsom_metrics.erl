b40f1f3b8cf0cd664b58a74dc30c470541576a22
Paul Peregud <paulperegud@gmail.com>
2012-06-02 19:42:05 +0200

    expose slide and slide_uniform histograms in folsom_metrics

--- src/folsom_metrics.erl
+++ src/folsom_metrics.erl
@@ -71,6 +71,12 @@ new_gauge(Name) ->
 new_histogram(Name) ->
     folsom_metrics:new_histogram(Name, ?DEFAULT_SAMPLE_TYPE, ?DEFAULT_SIZE, ?DEFAULT_ALPHA).
 
+new_histogram(Name, slide) ->
+    folsom_metrics:new_histogram(Name, slide, ?DEFAULT_SLIDING_WINDOW);
+
+new_histogram(Name, slide_uniform) ->
+    folsom_metrics:new_histogram(Name, slide_uniform, {?DEFAULT_SLIDING_WINDOW, ?DEFAULT_SIZE});
+
 new_histogram(Name, SampleType) ->
     folsom_metrics:new_histogram(Name, SampleType, ?DEFAULT_SIZE, ?DEFAULT_ALPHA).
 
